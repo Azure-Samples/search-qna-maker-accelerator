6b31624dcfd025b752b904d14941ff2493037dd1
Derek Legenzoff <dereklegenzoff@gmail.com>
2020-11-08 21:13:35 -0800

    finishing file upload page

--- search-ui/src/pages/Upload/Upload.js
+++ search-ui/src/pages/Upload/Upload.js
@@ -1,22 +1,79 @@
 import React, { useState } from "react";
+import axios from 'axios';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import "./Upload.css";
 
 export default function Upload() {
 
-  const [filePath, setFilePath] = useState("");
+  const [file, setFile] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   function onFileChange(event) {
-    let path = event.target.value;
+    let selectedFile = event.target.files[0];
+    console.log(selectedFile);
+    setFile(selectedFile);
 
-    // getting just the filename from the path
-    var fileName = path.split('\\').pop();
-
-    setFilePath(fileName);
+    setIsError(false);
+    setIsSuccess(false);
   }
 
-  function onUploadClick() {
+  async function onUploadClick() {
+    setIsLoading(true);
+    setIsError(false);
+    setIsSuccess(false);
 
+    var reader = new FileReader();
+
+    reader.onload = function () {
+      let base64File = reader.result.replace(/^data:.+;base64,/, '');
+
+
+      let body = {
+        name: file.name,
+        file: base64File
+      }
+
+      axios.post('/api/upload', body)
+        .then(response => {
+          setIsLoading(false);
+          setIsSuccess(true);
+        })
+        .catch(error => {
+          console.log(error);
+          setIsLoading(false);
+          setIsError(true);
+        });
+
+      console.log(body);
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+
+    reader.readAsDataURL(file);
+
+  }
+
+  var loading;
+  if (isLoading) {
+    loading = (<CircularProgress />);
+  }
+
+  var successMessage;
+  if (isSuccess) {
+    successMessage = (<div class="alert alert-success" role="alert">
+      Document successfully uploaded!
+    </div>);
+  }
+
+  var errorMessage;
+  if (isError) {
+    errorMessage = (<div class="alert alert-danger" role="alert">
+      Document upload failed :(
+    </div>);
   }
 
   return (
@@ -33,11 +90,16 @@ export default function Upload() {
           Select File
       </label>
         <input id="file-upload" type="file" onChange={(e) => onFileChange(e)} />
-        <p className="file-name">{filePath}</p>
+        <p className="file-name">{file.name}</p>
 
       </div>
       <br />
-      <button className="btn btn-primary rounded-0">Upload Document</button>
+      <button className="btn btn-primary rounded-0" onClick={onUploadClick}>Upload Document</button>
+      {loading}
+      <br />
+      <br />
+      {successMessage}
+      {errorMessage}
     </main>
   );
 };
